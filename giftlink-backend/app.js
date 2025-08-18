06f7e621c389597cb83a7b012ed18a1fc7a96bb5
Willie-Conway <hire.willie.conway@gmail.com>
2025-08-17 21:53:31 -0400

    Initial commit with Express gift search route

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,11 +24,8 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
-
+const searchRoutes = require('./routes/searchRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
-
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
@@ -39,7 +36,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 
 // Global Error Handler

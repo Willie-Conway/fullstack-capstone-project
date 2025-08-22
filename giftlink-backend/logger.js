7d33e2868ac6e48b02aefeb5da30964af792bb05
Willie  Conway <85038615+Willie-Conway@users.noreply.github.com>
2025-08-22 01:39:01 -0400

    Update logger.js

--- giftlink-backend/logger.js
+++ giftlink-backend/logger.js
@@ -1,3 +1,14 @@
+/* jshint esversion: 8 */
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
+
+// Get all gifts
+router.get('/', async (req, res, next) => {
+    // ...
+});
+
 const pino = require('pino');
 
 let logger;

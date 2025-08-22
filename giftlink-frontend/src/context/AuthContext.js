b4364324cbc4f23d62d76a7e5e9f06cdbbfa7abd
Willie  Conway <85038615+Willie-Conway@users.noreply.github.com>
2025-08-22 02:26:55 -0400

    Update AuthContext.js

--- giftlink-frontend/src/context/AuthContext.js
+++ giftlink-frontend/src/context/AuthContext.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
-const logger = require('../logger');
+
 
 // Get all gifts
 router.get('/', async (req, res, next) => {

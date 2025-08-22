01154793dbf8b338816a18512a61b4f8cc67fb3a
Willie  Conway <85038615+Willie-Conway@users.noreply.github.com>
2025-08-22 02:58:16 -0400

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,14 +1,4 @@
 /* jshint esversion: 8 */
-const express = require('express');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
-
-
-// Get all gifts
-router.get('/', async (req, res, next) => {
-    // ...
-});
-
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
@@ -21,25 +11,21 @@ import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import logger from './logger';
 
-
-
 function App() {
-  // eslint-disable-next-line no-unused-vars  
   const navigate = useNavigate();
   return (
     <>
-
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/details" element={<DetailsPage/>} />
-          <Route path="/app/search" element={<SearchPage/>} />
-
-        </Routes>
-        </>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/details" element={<DetailsPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
+      </Routes>
+    </>
   );
 }
+
 export default App;

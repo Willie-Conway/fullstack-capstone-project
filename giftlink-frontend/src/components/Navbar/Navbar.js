efb10e865c3f9d664c3944a2bddf137cc5c67598
Willie  Conway <85038615+Willie-Conway@users.noreply.github.com>
2025-08-22 03:13:27 -0400

    Update Navbar.js

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ import React, { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import {urlConfig} from '../../config';
 import { useAppContext } from '../../context/AuthContext';
-import logger from '../logger';
+
 
 
 export default function Navbar() {

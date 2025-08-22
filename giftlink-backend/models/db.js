e863aab0f739646df7441e5bfd4d9ce98e1a1837
Willie  Conway <85038615+Willie-Conway@users.noreply.github.com>
2025-08-22 01:37:27 -0400

    Update db.js

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,4 +1,15 @@
 // db.js
+/* jshint esversion: 8 */
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
+
+// Get all gifts
+router.get('/', async (req, res, next) => {
+    // ...
+});
+
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
